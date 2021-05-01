5d6444fb641557e1f083c473968d0540cc57b5ca
Myrotvorets <support@myrotvorets.center>
2021-05-01 14:31:23 +0300

    Update dependencies

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -2,6 +2,7 @@
 
 import { EventEmitter } from 'events';
 import { OpenTelemetryConfigurator } from '@myrotvorets/opentelemetry-configurator';
+// import { KnexPlugin } from '@myrotvorets/opentelemetry-plugin-knex';
 
 if (+(process.env.ENABLE_TRACING || 0)) {
     EventEmitter.defaultMaxListeners += 5;
@@ -11,15 +12,7 @@ export async function configure(): Promise<void> {
     if (+(process.env.ENABLE_TRACING || 0)) {
         const configurator = new OpenTelemetryConfigurator({
             serviceName: 'psb-api-identigraf-auth',
-            instrumentations: [
-                {
-                    plugins: {
-                        knex: {
-                            path: '@myrotvorets/opentelemetry-plugin-knex',
-                        },
-                    },
-                },
-            ],
+            // instrumentations: [new KnexPlugin()],
         });
 
         await configurator.start();
