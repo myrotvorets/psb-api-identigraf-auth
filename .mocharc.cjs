060f9c886e392d540ff7d558e011efc9466e28b8
Myrotvorets <support@myrotvorets.center>
2023-09-18 18:38:44 +0300

    Update .mocharc.cjs

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -7,6 +7,6 @@ module.exports = {
     'reporter-option': [
         'spec=-',
         process.env.GITHUB_ACTIONS === 'true' ? 'mocha-reporter-gha=-' : null,
-        'mocha-reporter-sonarqube=test-report.xml'
+        process.env.SONARSCANNER === 'true' ? 'mocha-reporter-sonarqube=test-report.xml' : null,
     ].filter(Boolean),
 }
